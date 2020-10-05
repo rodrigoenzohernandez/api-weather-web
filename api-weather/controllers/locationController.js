4c58cca40e3f14b7118a1f17c180f1dd3a6e2fcf
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2020-10-05 02:18:51 -0300

    refactor: delete unnecessary function

--- api-weather/controllers/locationController.js
+++ api-weather/controllers/locationController.js
@@ -1,22 +1,16 @@
-async function searchWeather(req, res) {
-    try {
-        const fetch = require("node-fetch");
-        var geoip = require('geoip-lite');
-        let data = await fetch("https://api.myip.com/");
-        let contenido = await data.json();
-        let city = geoip.lookup(contenido.ip);
-        return res.json(city);
-
-    }
-    catch (err) {
-        console.log(err);
-    }
-}
-
 const controller = {
-    showLocation: function (req, res, next) {
-        return searchWeather(req, res);
-    },
+    showLocation: async function (req, res) {
+        try {
+            const fetch = require("node-fetch");
+            var geoip = require('geoip-lite');
+            let data = await fetch("https://api.myip.com/");
+            let contenido = await data.json();
+            let cityData = await geoip.lookup(contenido.ip);
+            return res.json(cityData);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
 }
-
 module.exports = controller;
