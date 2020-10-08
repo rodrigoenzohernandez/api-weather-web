98b25990a58fda9858f3d688929fb183ee04d1d0
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2020-10-08 02:16:08 -0300

    test

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -4,9 +4,9 @@ const controller = {
             var geoip = require('geoip-lite');
      
             let ip = req.headers['x-real-ip'];
-            return res.send(ip);
-            //let cityData = geoip.lookup(ip);
-            //return res.json(cityData);
+            //return res.send(ip);
+            let cityData = geoip.lookup(ip);
+            return res.json(cityData);
         }
         catch (err) {
             console.log(err);
