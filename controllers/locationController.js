4e4ebb5c80ab1791fa34034bde45ea012591b439
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2020-10-08 01:53:39 -0300

    test new ip

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -3,9 +3,16 @@ const controller = {
         try {
             var geoip = require('geoip-lite');
             //let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-            let ip = socket.handshake.address;
-            let cityData = geoip.lookup(ip);
-            return res.json(cityData);
+            //let ip = socket.handshake.address;
+
+            var io = require('socket.io').listen(80);
+            io.sockets.on('connection', function (socket) {
+                var endpoint = socket.manager.handshaken[socket.id].address;
+                console.log('Client connected from: ' + endpoint.address + ":" + endpoint.port);
+            });
+            //res.data(endpoint.address);
+            ///let cityData = geoip.lookup(endpoint.address);
+            return res.json(endpoint.address);
         }
         catch (err) {
             console.log(err);
