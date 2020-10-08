efbab07f2b26b6cb1b9943892ab1ab56a5f7066a
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2020-10-08 02:00:06 -0300

    test

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -2,17 +2,9 @@ const controller = {
     showLocation: async function (req, res) {
         try {
             var geoip = require('geoip-lite');
-            //let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
-            //let ip = socket.handshake.address;
-
-            var io = require('socket.io').listen(80);
-            io.sockets.on('connection', function (socket) {
-                var endpoint = socket.manager.handshaken[socket.id].address;
-                console.log('Client connected from: ' + endpoint.address + ":" + endpoint.port);
-            });
-            //res.data(endpoint.address);
-            ///let cityData = geoip.lookup(endpoint.address);
-            return res.json(endpoint.address);
+            let ip = req.ip;
+            let cityData = geoip.lookup(ip);
+            return res.json(cityData);
         }
         catch (err) {
             console.log(err);
@@ -20,5 +12,3 @@ const controller = {
     }
 }
 module.exports = controller;
-
-
