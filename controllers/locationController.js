9a8b7cdaf51e55d95b914944d736baf15759aa8a
rodrigoenzohernandez <rodrigoenzohernandez@gmail.com>
2020-10-08 02:08:01 -0300

    test

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -2,8 +2,9 @@ const controller = {
     showLocation: async function (req, res) {
         try {
             var geoip = require('geoip-lite');
-            let ip = req.ip;
-            let cityData = geoip.lookup(ip);
+            const ipFromRequest = requestIp.getClientIp(req).split(':').slice(-1).pop()
+              res.send(ipFromRequest)
+            //let cityData = geoip.lookup(ip);
             return res.json(cityData);
         }
         catch (err) {
